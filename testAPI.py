d16e7c66b0cd88d0fefca9eaf39a49524576ee38
locwyn <36432201+locwyn@users.noreply.github.com>
2018-02-13 19:39:43 -0600

    Added keys/tokens
    Added twitter consumer key, consumer secret, access token, and access token secret

--- testAPI.py
+++ testAPI.py
@@ -3,8 +3,8 @@ from tweepy import OAuthHandler
 
 consumer_key = 'dgmlPcoo2F3Ws6LWJLmuTgOVb'
 consumer_secret = 'cME0sSMXOXHO3qG06tEwSyejzvlYlqLzx1ynKRBqGjbuYRIOZz'
-access_token = ''
-access_secret = ''
+access_token = '903791836791267328-otZ5372pWtzUDvSEOU9IWeSyPojrnz5'
+access_secret = 'cfCMYG8815czjkRljOo1HAvg09pvu9GPyFVMmfKZOI4XD'
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
