60781415cb351fa56a29a3daad20f2af1ab87945
locwyn <locwyn@protonmail.com>
2018-02-14 02:56:11 +0000

    Added api test logic

--- testAPI.py
+++ testAPI.py
@@ -10,3 +10,7 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
+
+for status in tweepy.Cursor(api.home_timeline).items(10):
+  #Process a single status
+  print(status.text)
