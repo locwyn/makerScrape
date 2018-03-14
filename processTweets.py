688fa921d3a5dc8cfa425a557e9525c4e3bb803c
locwyn <locwyn@protonmail.com>
2018-03-14 04:19:02 +0000

    starting to use processTweets.py to test data

--- processTweets.py
+++ processTweets.py
@@ -1,8 +1,13 @@
 import json
 
-with open('2018_02_28_maker.json') as f:
+with open('2018_03_01_maker.json') as f:
   tweets = f.readlines()
+
+count = 0
 
 for x in tweets:
   tjson = json.loads(x)
-  print(tjson['created_at'])
+  if (tjson['retweeted_status']):
+    count += 1
+
+print(count)
