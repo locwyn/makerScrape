4437fe73e96472aa4adb054346ea2462d344ce00
locwyn <locwyn@protonmail.com>
2018-03-16 22:32:55 +0000

    further testing of data via processTweets.py

--- processTweets.py
+++ processTweets.py
@@ -1,13 +1,21 @@
 import json
 
-with open('2018_03_01_maker.json') as f:
+with open('2018_03_14_maker.json') as f:
   tweets = f.readlines()
 
 count = 0
-
+"""
 for x in tweets:
   tjson = json.loads(x)
-  if (tjson['retweeted_status']):
+  if tjson['user']['followers_count'] >= 2000:  
+    try:
+      tjson['retweeted_status']
+    except KeyError:    
+      count += 1
+"""
+for y in tweets:
+#  tjson = json.loads(y)
+  if 'retweeted_status' in y:
     count += 1
 
 print(count)
