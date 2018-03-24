4c6f0bfa58b38426b2b7e0f4da1bad4bee128bd2
locwyn <locwyn@protonmail.com>
2018-03-24 00:01:39 +0000

    added time limit to processStream.py

--- processStream.py
+++ processStream.py
@@ -1,5 +1,6 @@
 import tweepy
 import datetime
+import time
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
@@ -14,27 +15,27 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-#for status in tweepy.Cursor(api.home_timeline).items(10):
-#  #Process a single status
-#  print(status.text)
-
-#testing streaming
 class MyListener(StreamListener):
+
+  def __init__(self, start_time, time_limit=60):
+    self.time = start_time
+    self.limit = time_limit
 
   def on_data(self, data):
     jsonFile = datetime.datetime.now().strftime('%Y_%m_%d') + '_maker.json'
-    try:
-      with open(jsonFile, 'a') as f:
-        f.write(data)
-        return True
-    except BaseException as e:
-      print("Error on_data: %s" % str(e))
-    return True
+    while (time.time() - self.time)  self.limit:
+      try:
+        with open(jsonFile, 'a') as f:
+          f.write(data)
+          return True
+      except BaseException as e:
+        print("Error on_data: %s" % str(e))
+      return True
 
   def on_error(self, status):
     print(status)
     return True
-
-twitter_stream = Stream(auth, MyListener())
+startTime = time.time()
+twitter_stream = Stream(auth, MyListener(startTime))
 twitter_stream.filter(track=['#maker'])
 
