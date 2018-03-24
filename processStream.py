07748bd6c190a606ba47491cf027053f4551174c
locwyn <locwyn@protonmail.com>
2018-03-24 02:15:44 +0000

    changed file path to data files

--- processStream.py
+++ processStream.py
@@ -14,16 +14,17 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
-
+filePath = '/home/gbk/data/makerScrape/'
 class MyListener(StreamListener):
 
-  def __init__(self, start_time, time_limit=60):
+  def __init__(self, start_time, time_limit=2700):
     self.time = start_time
     self.limit = time_limit
 
   def on_data(self, data):
-    jsonFile = datetime.datetime.now().strftime('%Y_%m_%d') + '_maker.json'
+    jsonFile = filePath + datetime.datetime.now().strftime('%Y_%m_%d') + '_maker.json'
     while (time.time() - self.time) < self.limit:
+      print(str(time.time() - self.time) + ' ' + str(self.limit))
       try:
         with open(jsonFile, 'a') as f:
           f.write(data)
@@ -31,6 +32,7 @@ class MyListener(StreamListener):
       except BaseException as e:
         print("Error on_data: %s" % str(e))
       return True
+    return False
 
   def on_error(self, status):
     print(status)
