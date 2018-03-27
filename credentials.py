7f0072789401982fa5c018fa03c042c179c942e6
locwyn <36432201+locwyn@users.noreply.github.com>
2018-03-27 18:32:25 -0500

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,4 @@
+consumer_key = 'dgmlPcoo2F3Ws6LWJLmuTgOVb'
+consumer_secret = 'cME0sSMXOXHO3qG06tEwSyejzvlYlqLzx1ynKRBqGjbuYRIOZz'
+access_token = '903791836791267328-otZ5372pWtzUDvSEOU9IWeSyPojrnz5'
+access_secret = 'cfCMYG8815czjkRljOo1HAvg09pvu9GPyFVMmfKZOI4XD'
