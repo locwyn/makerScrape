6da51c39339538156abf6c7ef658e1141c355ed4
locwyn <locwyn@protonmail.com>
2018-03-28 01:09:00 +0000

    first test of pullTweets.py

--- pullTweets.py
+++ pullTweets.py
@@ -11,29 +11,33 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 filePath = '/home/gbk/data/makerScrape/'
-class MyListener(StreamListener):
 
-  def __init__(self, start_time, time_limit=2700):
-    self.time = start_time
-    self.limit = time_limit
+#class MyListener(StreamListener):
+#
+#  def __init__(self, start_time, time_limit=2700):
+#    self.time = start_time
+#    self.limit = time_limit
+#
+#  def on_data(self, data):
+#    jsonFile = filePath + datetime.datetime.now().strftime('%Y_%m_%d') + '_maker.json'
+#    while (time.time() - self.time) < self.limit:
+#      try:
+#        with open(jsonFile, 'a') as f:
+#          f.write(data)
+#          return True
+#      except BaseException as e:
+#        print("Error on_data: %s" % str(e))
+#      return True
+#    return False
+#
+#  def on_error(self, status):
+#    print(status)
+#    return True
 
-  def on_data(self, data):
-    jsonFile = filePath + datetime.datetime.now().strftime('%Y_%m_%d') + '_maker.json'
-    while (time.time() - self.time) < self.limit:
-      try:
-        with open(jsonFile, 'a') as f:
-          f.write(data)
-          return True
-      except BaseException as e:
-        print("Error on_data: %s" % str(e))
-      return True
-    return False
+#startTime = time.time()
+#twitter_stream = Stream(auth, MyListener(startTime))
+#twitter_stream.filter(track=['#maker'])
+#twitter_stream.disconnect()
 
-  def on_error(self, status):
-    print(status)
-    return True
-
-startTime = time.time()
-twitter_stream = Stream(auth, MyListener(startTime))
-twitter_stream.filter(track=['#maker'])
-twitter_stream.disconnect()
+for tweet in tweepy.Cursor(api.search,q='#maker').items(10):
+  print('Tweet by: @' + tweet.user.screen_name)
