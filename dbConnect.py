f520f48daae15a94f1a6637b61fafd76f78d6a46
locwyn <36432201+locwyn@users.noreply.github.com>
2018-04-18 20:08:13 -0500

    Adding Error Handling to DB connection

--- dbConnect.py
+++ dbConnect.py
@@ -5,7 +5,11 @@ def databaseConnect():
   return mysql.connector.connect(user=dbUser, password=dbPassword, 
            host=dbHost, database='makerTweets', charset='utf8mb4',
            collation='utf8mb4_unicode_ci')
-
-cnx = databaseConnect()
-
+try:
+  cnx = databaseConnect()
+except mysql.connector.Error as e:
+  print("Error Code: " + str(e.errno))
+  print("SQLSTATE Value: " + e.sqlstate)
+  print("Error Message: " + e.msg)
+  print("Error: " + e)
 cnx.close()
