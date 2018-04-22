e30cf9acd88df273690413a0de49cda7a90af8b8
locwyn <locwyn@protonmail.com>
2018-04-22 01:43:55 +0000

    Adjusting error logging

--- dbConnect.py
+++ dbConnect.py
@@ -7,15 +7,19 @@ def databaseConnect():
   return mysql.connector.connect(user=dbUser, password=dbPassword, 
            host=dbHost, database='takerTweets', charset='utf8mb4',
            collation='utf8mb4_unicode_ci')
+
+def writeErrorLog(e):
+
+
 try:
   cnx = databaseConnect()
   cnx.close()
 except mysql.connector.Error as e:
   errorFile = datetime.datetime.now().strftime('%Y_%m_%d') + '_error.log'
-    try:
-      with open(errorFile, 'a') as f:
-        f.write(str(e))
-    except BaseException as e:
-      print("Error on_data: %s" % str(e))
+  try:
+    with open(errorFile, 'a') as f:
+      f.write(str(e))
+  except BaseException as e:
+    print("Error on_data: %s" % str(e))
 
 
