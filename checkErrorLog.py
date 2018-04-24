cc2d717844f86358e08f004e98b73b6bf3f957c0
locwyn <locwyn@protonmail.com>
2018-04-24 02:05:42 +0000

    File evaluation logic added
    checkErrorLogs can now determine if an error log exists and is an
    hour old or not, email logic needs added next

--- checkErrorLog.py
+++ checkErrorLog.py
@@ -1,4 +1,4 @@
-
+#!/user/bin/env python3
 import os
 import sys
 import time
@@ -6,9 +6,14 @@ import datetime
 
 def checkForNewErrorLog(fileName):
   if os.path.isfile(fileName):
-    print("New Error Log Exists")
+    timeAnHourAgo = time.time() - (60 * 60)
+    stat = os.stat(fileName)
+    if stat.st_mtime >= timeAnHourAgo:
+      print("Updated Error Log Exists")
+    else:
+      print("Old Error Log Exists")
   else:
-    print("No, New Logs")
+    print("No Error Log Exists")
 
 if __name__ == "__main__":
   fileName = datetime.datetime.now().strftime('%Y_%m_%d') + '_error.log'
