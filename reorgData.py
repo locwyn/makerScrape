1921a2c0153a3e55024337a476d080d231eae348
locwyn <36432201+locwyn@users.noreply.github.com>
2018-04-29 20:19:09 -0500

    Copying in useful code from other programs

--- reorgData.py
+++ reorgData.py
@@ -1,2 +1,35 @@
 #!/bin/dev/env python
 
+import mysql.connector
+from credentials import *
+
+def databaseConnect():
+  return mysql.connector.connect(user=dbUser, password=dbPassword, 
+           host=dbHost, database='makerTweets', charset='utf8mb4',
+           collation='utf8mb4_unicode_ci')
+
+def checkDatabaseForItem(selectQuery):
+  cnx = databaseConnect()
+  cursor = cnx.cursor()
+  try:
+    cursor.execute(selectQuery)
+    results = 0
+    for x in cursor:
+      results += 1
+    cnx.close()
+  except mysql.connector.Error as e:
+    writeErrorLog(e)
+  return results
+
+def writeErrorLog(e):
+  filePath = '/home/gbk/data/makerScrape/logs/'
+  errorFile = (filePath + datetime.datetime.now().strftime('%Y_%m_%d') + 
+              '_reorg_error.log')
+  try:
+    with open(errorFile, 'a') as f:
+      f.write(str(e))
+  except BaseException as e:
+    with open(errorFile, 'a') as f:
+      f.write("Unable to write error")
+        
+if __name__ == "__main__":
