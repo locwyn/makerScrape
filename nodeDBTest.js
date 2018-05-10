90ab644bf3e248721325eda361dc5a18d0eb12d5
locwyn <locwyn@protonmail.com>
2018-05-10 00:22:19 +0000

    Testing first node.js program
    installed node.js to handle mysql queries on the server

---
+++ nodeDBTest.js
@@ -0,0 +1,13 @@
+var credentials = require('./credentials');
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: credentials.dbHost,
+  user: credentials.dbUser,
+  password: credentials.dbPassword
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
