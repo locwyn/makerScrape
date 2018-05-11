a0ed10f5f0632dbcdb55685ceac30d816319ae45
locwyn <36432201+locwyn@users.noreply.github.com>
2018-05-10 21:13:21 -0500

    Adding a test query to pull 10 most recent tweets

--- nodeDBTest.js
+++ nodeDBTest.js
@@ -4,10 +4,15 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: credentials.dbHost,
   user: credentials.dbUser,
-  password: credentials.dbPassword
+  password: credentials.dbPassword,
+  database: "makerTweets"
 });
 
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
+  con.query("SELECT TOP 10 tweetID FROM tweets ORDER BY id DESC",
+            function (err, results, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
 });
