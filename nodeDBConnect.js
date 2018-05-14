1a12cc514579be9260e2dc6e63130bba2ddd92e5
locwyn <locwyn@protonmail.com>
2018-05-14 23:35:51 +0000

    More testing node mysql connection

--- nodeDBConnect.js
+++ nodeDBConnect.js
@@ -5,8 +5,8 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: credentials.dbHost,
   user: credentials.dbUser,
-  password: credentials.dbPassword
-//  database: "makerTweets"
+  password: credentials.dbPassword,
+  database: "makerTweets"
 });
 
 con.connect(function(err) {
@@ -16,9 +16,9 @@ con.connect(function(err) {
 
 //console.log("Success!");
 
-con.end(function(err) {
-  if (err) console.log("End broke!");
-  console.log("Ended!");
-});
+//con.end(function(err) {
+//  if (err) console.log("End broke!");
+//  console.log("Ended!");
+//});
 
 //console.log("Ended!");
